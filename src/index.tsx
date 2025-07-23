d1f256e86927d5255f2eff63fdc60bd5fc936f0f
Akeem Shaw <48392703+TrebleKlefy@users.noreply.github.com>
2025-07-23 10:25:46 -0500

    m

--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,7 @@ if (typeof window !== 'undefined') {
         try{
             await initializeWidgetConfig();
             let data = getWidgetConfig().domains.includes(window.location.origin)
+            console.log('this is the window that origin',window.location.origin)
             console.log(getWidgetConfig().domains,'this is the list of domains')
              console.log('this is data in the initializeWidgetConfig',data)
             if(data){
